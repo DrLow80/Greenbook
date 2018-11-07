513de9a294cb7596dcbb791e7003dc75c4581c8d
Matthew Mitchell <Matthew.Mitchell@Aurora.org>
2018-11-07 11:52:58 -0600

    fixed project type for Sessions, added visibility convert logic for referenced content view

--- Greenbook/Greenbook.Sessions/ContentItemsViewModel.cs
+++ Greenbook/Greenbook.Sessions/ContentItemsViewModel.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 using Greenbook.Domain;
 using Greenbook.Entities;
 
@@ -10,7 +8,18 @@ namespace Greenbook.Sessions
 {
     public class ContentItemsViewModel : BaseViewModel
     {
+        private readonly ISessionRepository _sessionRepository;
+
+        public ContentItemsViewModel(ISessionRepository sessionRepository)
+        {
+            _sessionRepository = sessionRepository;
+        }
+
         public ObservableCollection<ContentItem> ContentItems { get; } = new ObservableCollection<ContentItem>();
+
+        public ObservableCollection<ContentItemsByTypeGroup> ContentItemsByTypeGroups { get; } =
+            new ObservableCollection<ContentItemsByTypeGroup>();
+
         public ObservableCollection<ContentItem> ScannedContentItems { get; } = new ObservableCollection<ContentItem>();
 
         public void Load()
@@ -18,27 +27,32 @@ namespace Greenbook.Sessions
             ContentItems.Clear();
 
             foreach (var contentItem in _sessionRepository.LoadContentItems().OrderBy(x => x.Name))
-            {
                 ContentItems.Add(contentItem);
-            }
 
             ContentItemsByTypeGroups.Clear();
 
             foreach (var type in ContentItems.GroupBy(x => x.ContentType).OrderBy(x => x.Key))
             {
-                ContentItemsByTypeGroup contentItemsByTypeGroup = new ContentItemsByTypeGroup(type.Key, type.OrderBy(x => x.Name));
+                var contentItemsByTypeGroup = new ContentItemsByTypeGroup(type.Key, type.OrderBy(x => x.Name));
 
                 ContentItemsByTypeGroups.Add(contentItemsByTypeGroup);
             }
         }
 
-        private ISessionRepository _sessionRepository;
+        public ICommand ChangeViewCommand => new RelayCommand<ViewType>(OnChangeView);
 
-        public ContentItemsViewModel(ISessionRepository sessionRepository)
+        private void OnChangeView(ViewType obj)
         {
-            _sessionRepository = sessionRepository;
+            ViewType = obj;
         }
 
-        public ObservableCollection<ContentItemsByTypeGroup> ContentItemsByTypeGroups { get; } = new ObservableCollection<ContentItemsByTypeGroup>();
+        public ViewType ViewType { get; private set; } = ViewType.Referenced;
+    }
+
+    public enum ViewType
+    {
+        All,
+        Grouped,
+        Referenced
     }
 }
---
+++ Greenbook/Greenbook.Sessions/ViewTypeToVisibilityConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Greenbook.Sessions
+{
+    public class ViewTypeToVisibilityConverter : ViewTypeToBoolConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DoesViewTypeMatch(value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+
+    public class ViewTypeToBoolConverter : MarkupExtension, IValueConverter
+    {
+        public ViewType ViewType { get; set; } = ViewType.All;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DoesViewTypeMatch(value);
+        }
+
+        public bool DoesViewTypeMatch(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            var enumValue = value.ToString();
+
+            if (string.IsNullOrEmpty(enumValue))
+            {
+                return false;
+            }
+
+            var parsed = Enum.TryParse(value.ToString(), true, out ViewType result);
+
+            if (!parsed)
+            {
+                return false;
+            }
+
+            return result == ViewType;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
