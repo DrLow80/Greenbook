d655261f7869330e18b69b7a366c02901391ff80
Matthew Mitchell <mmitchell80@yahoo.com>
2019-02-17 16:37:07 -0600

    WIP creating mention control

---
+++ Greenbook/Greenbook.Feature.ContentMentionTextBox/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Greenbook.Feature.ContentMentionTextBox.Converters
+{
+    public class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public Visibility TrueVisibility { get; set; } = Visibility.Visible;
+        public Visibility FalseVisibility { get; set; } = Visibility.Collapsed;
+
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return FalseVisibility;
+            }
+
+            var stringValue = value.ToString();
+
+            if (string.IsNullOrWhiteSpace(stringValue))
+            {
+                return FalseVisibility;
+            }
+
+            bool result = bool.TryParse(stringValue, out var boolResult);
+
+            if (!result)
+            {
+                return FalseVisibility;
+            }
+
+            return boolResult ? TrueVisibility : FalseVisibility;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+
+    }
+}
